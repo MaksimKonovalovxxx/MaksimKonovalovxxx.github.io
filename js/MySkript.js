2b15a183b6450a5bcebf08405a97147567f7dbf7
MaksimKonovalovxxx <103588564+MaksimKonovalovxxx@users.noreply.github.com>
2022-05-28 16:16:01 +0400

    Add files via upload
    Исправил ошибку кнопка навигации, и медия запросы

---
+++ js/MySkript.js
@@ -0,0 +1,11 @@
+"use strict"
+
+if (confirm ("Привет?")){
+    console.log("Приветики");}
+
+
+let age = prompt("Сколько тебе лет?", "5");
+console.log(age);
+
+let goi = prompt("Как тебя зовут?");
+console.log(goi);
